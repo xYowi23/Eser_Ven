3fc17708839939655a9f5e1ca3d10719edffa362
Luis Rodriguez <luisyowi2317@gmail.com>
2024-09-13 17:43:15 +0200

    fine_esercizio

---
+++ Task_13_03_Eser2/Task_13_03_Eser2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+using Task_13_03_Eser2.classes;
+
+namespace Task_13_03_Eser2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Creare un'applicazione console che permetta la gestione di una lista di studenti,con la possibilta di inserire 
+             * nuovi studenti, modificare i dati, visualizzarli e filtrare la lista in base ai voti.
+             
+             */
+
+            List <string> studenti = new List <string> ();
+
+                      
+
+            Console.WriteLine("Nome:");
+            string? varNome = Console.ReadLine();
+            Console.WriteLine("Cognome:");
+            string? varCognome = Console.ReadLine();
+            Console.WriteLine("Voto:");
+            double varVoto = Convert.ToDouble(Console.ReadLine());
+
+            Studente stu = new Studente();
+            stu.Nome = varNome;
+            stu.Cognome = varCognome;
+            stu.Voto = varVoto;
+              
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Task_13_03_Eser2/Task_13_03_Eser2/classes/Studente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_03_Eser2.classes
+{
+    internal class Studente
+    {
+
+        public string Nome {  get; set; }
+        public string Cognome {  get; set; }
+        public double Voto { get; set; }
+
+        public Studente() { }
+
+
+
+        public void StampaDettaglio()
+        {
+            Console.WriteLine($"Nome: {Nome}\n Cognome: {Cognome}\n Voto: {Voto}\n");
+        }
+
+    }
+}
